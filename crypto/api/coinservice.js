6ed5ee222b19c1190f9409fdf0ec361cbf39a6f2
avrokj <jan.korva@gmail.com>
2023-05-24 08:58:27 +0300

    updated

--- crypto/api/coinservice.js
+++ crypto/api/coinservice.js
@@ -1,22 +1,35 @@
-import { API_KEY, BASE_URL, RAPID_API_HOST } from "../environment.js";
+import { API_KEY, BASE_URL, RAPID_API_HOST } from "../environment.js"; // impordime sisse failist tundlikud andmed, et saakisme kasutada
 
-const options = {
+const settings = {
   'X-RapidAPI-Key': API_KEY,
   "X-RapidAPI-Host": RAPID_API_HOST,
 }
 
 export const getCoins = async () => {
   try {
-    const response = await fetch(`${BASE_URL}/coins`, { headers: options });
+    const response = await fetch(`${BASE_URL}/coins`, { headers: settings });
     return response.json();
   } catch (error) {
     console.log(error);
   }
 }
 
-export const getCoin = async () => {
+export const getCoin = async (uuid) => { // uuid on placeholder ja võib olla tegelt, mis iganes
   try {
-    const response = await fetch(`${BASE_URL}/coin`, { headers: options });
+    const response = await fetch(`${BASE_URL}/coin/${uuid}`, {
+      headers: settings,
+    });
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const getCoinHistory = async (uuid) => {
+  try {
+    const response = await fetch(`${BASE_URL}/coin/${uuid}/history`, {
+      headers: settings,
+    });
     return response.json();
   } catch (error) {
     console.log(error);
--- crypto/src/coin.js
+++ crypto/src/coin.js
@@ -1,24 +1,30 @@
-import { getCoins } from "../api/coinservice.js";
+import { getCoins, getCoin, getCoinHistory } from "../api/coinservice.js";
 
 const data = await getCoins();    // kui teame et functsioon on async, siis peab ootama
-const datacoin = await getCoin();
+//const datacoin = await getCoin();
+//console.log(datacoin.datacoin.coin);
 
-//  .then((data) => console.log(data));
+//.then((datacoin) => console.log(datacoin));
 const marketTrendsWrapper = document.querySelector("#market_trends");
 console.log(marketTrendsWrapper);
-console.log(data.data.coins);
-console.log(data.datacoin.coin);
+//console.log(datacoin.data.coin);
+
 
 data.data.coins.forEach((coin) => {
   const article = document.createElement("article");
+
+  article.setAttribute("data-uuid", coin.uuid)
+
   const articleClasses = [
     "border-2",
     "p-4",
     "rounded-2xl"
   ];
+
   const borderColorsWeDontWant = ['#000000', '#00042b'];
   article.style.borderColor = borderColorsWeDontWant.includes(coin.color) ? "#73FDAA" : coin.color;
   article.classList.add(...articleClasses);
+
   article.innerHTML = `
           <div class="flex mb-2">
             <div class="flex w-4/5 items-center">
@@ -28,10 +34,29 @@ data.data.coins.forEach((coin) => {
             </div>
             <img class="mx-auto mt-4 h-4 v-4" src="assets/arrow.png" alt="">
           </div>
-          <div class="grid gap-2 px-4 pt-4 pb-2">
-            <p class="text-2xl">$${Number(coin.price).toFixed(2)}</p>
-            <p>${coin.change}%</p>
+          <div class="grid grid-cols-2 gap-2 px-4 pt-4 pb-2">
+            <div>
+              <p class="text-2xl">$${Number(coin.price).toFixed(2)}</p>
+              <p class="${String(coin.change).startsWith('-') ? 'text-red-500' : 'text-green-500'}">${coin.change}%</p>
+            </div>
+            <div>
+              <p>${coin.uuid}%</p>
+            </div>
           </div>
         `;
   marketTrendsWrapper.append(article);
 });
+
+const cards = document.querySelectorAll("#market_trends article")
+//console.log(cards);
+cards.forEach((node) => {
+  node.addEventListener('click', async (event) => {
+    const uuid = event.currentTarget.getAttribute("data-uuid");
+    await getCoin(uuid); // await küsib eelnevalt, et oleks async eespool olemas!!!
+    const response = await getCoin(uuid);
+    const history = await getCoinHistory(uuid);
+    const result = {...response.data.coin, ... history.data}; // 3 täppi sellesks, et saada kogu data (spread operator). see rida paneb, siis Coini ja history ühte jurasse kokku.
+    console.log(result);
+  })
+}
+);
